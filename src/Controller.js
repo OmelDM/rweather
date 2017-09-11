2e3cc823c9713100e6098e15df9761ab63a5ebb6
Dmytro Omelchuk <omeldm@gmail.com>
2017-09-11 21:41:06 +0300

    Implemented basic fetchig of weather data

--- src/Controller.js
+++ src/Controller.js
@@ -1,7 +1,33 @@
+const GEOLOCATION_BY_IP_URL = 'http://ip-api.com/json';
+const APIXU_API_KEY = '7297b637fde44b36ad2153805171109';
+
+function fetchJSON(response) {
+    if (!response.ok) {
+        throw new Error(`${response.status}: ${response.statusText}`);
+    }
+
+    return response.json();
+}
+
 export default class Controller {
     fetchWeather() {
         return new Promise((resolve, reject) => {
-            resolve({id: 22,name:'Some'});
+
+            fetch(GEOLOCATION_BY_IP_URL)
+                .then(fetchJSON)
+                .then(locationJSON => {
+                    // parse json to get current lat lon
+                    return fetch(`http://api.apixu.com/v1/current.json?key=${APIXU_API_KEY}&q=Kiev`);
+                })
+                .then(fetchJSON)
+                .then(weatherJSON => {
+                    // parse json to local Weather object
+                    const weather = weatherJSON.current.temp_c;
+                    resolve(weather);
+                })
+                .catch(error => {
+                    reject(error);
+                });
         });
     }
 
